6dfb5f8549bf100f3483095e1a6e7b6de6421307
Preshant Achuthan <engerium@gmail.com>
2023-11-29 12:01:49 +0800

    feat: Added save booking function in actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,10 @@
 'use server';
+
+const { PrismaClient } = require('@prisma/client')
  
 export async function createBooking(formData: FormData) {
+    const prisma = new PrismaClient();
+
     const rawFormData = {
         bookingTitle: formData.get('bookingTitle'),
         bookingDay: formData.get('bookingDay'),
@@ -8,7 +12,24 @@ export async function createBooking(formData: FormData) {
         bookingYear: formData.get('bookingYear'),
         bookingStartTime: formData.get('bookingStartTime'),
         bookingEndTime: formData.get('bookingEndTime'),
-      };
+    };
 
-      console.log(rawFormData);
+    // Save booking to database
+    try {
+      const booking = await prisma.booking.create({
+          data: {
+              title: rawFormData.bookingTitle,
+              day: rawFormData.bookingDay,
+              month: rawFormData.bookingMonth,
+              year: rawFormData.bookingYear,
+              startTime: rawFormData.bookingStartTime,
+              endTime: rawFormData.bookingEndTime,
+          },
+      });
+      console.log('Booking created:', booking);
+    } catch (error: any) {
+        console.error(`Error creating booking: ${error.message}`);
+    } finally {
+        await prisma.$disconnect();
+    }
 }
